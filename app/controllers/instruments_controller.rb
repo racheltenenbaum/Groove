4a3bf1d67be830398c8a0884fef9ab28a1af560b
Rachel Tenenbaum <racheltenenbaum14@gmail.com>
2023-08-22 16:08:33 +0100

    instruments done except for user speicific functionality

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -13,11 +13,33 @@ class InstrumentsController < ApplicationController
 
   def create
     @instrument = Instrument.new(instrument_params)
+    @instrument.user_id = current_user.id
+    if @instrument.save
+      redirect_to instruments_path
+    end
+  end
+
+  def edit
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def update
+    @instrument = Instrument.find(params[:id])
+    @instrument.update(instrument_params)
+    if @instrument.save
+      redirect_to instrument_path(@instrument.id)
+    end
+  end
+
+  def destroy
+    @instrument = Instrument.find(params[:id])
+    @instrument.destroy
+    redirect_to instruments_path
   end
 
   private
 
   def instrument_params
-    params.require(:instrument).permit(:name, :available_from, :description, :brand, :price, :city, :user_id)
+    params.require(:instrument).permit(:name, :available_from, :description, :brand, :price, :city)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'instruments/index'
-  get 'instruments/show'
-  get 'instruments/new'
-  get 'instruments/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :instruments, only: [:index, :show, :new, :create]
+  resources :instruments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
