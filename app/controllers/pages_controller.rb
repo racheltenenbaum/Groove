48cb22e38d81ef4989e57a1d49b0f4c69b515cad
Rachel Tenenbaum <racheltenenbaum14@gmail.com>
2023-08-23 12:20:14 +0100

    destroy edits and fine tuning pre dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @booking = Booking.where(current_user.id == booking.user_id)
+  end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,7 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+
+  # def self.owner_name
+  #   User.find(instrument.user_id).username
+  # end
 end
