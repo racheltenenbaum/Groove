7e38fd8257d717b45d2dbb4d4d87b155f34def99
Jurgest Hajrullaku <hajrullakuj@gmail.com>
2023-08-23 14:39:48 +0100

    added some stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
 Booking.destroy_all
 Instrument.destroy_all
@@ -15,28 +16,13 @@ user_one = User.create!(email: "test6@test.com", password: "123456", username: "
 user_two = User.create!(email: "test5@test.com", password: "123456", username: "testguy1")
 
 #Instruments for user_one
-guitar = Instrument.new(available_from: "1889/03/03",name: "guitar", description: "very good", brand: "bosch", city: "London", price: 300 )
-piano = Instrument.new(available_from: "1889/03/03",name: "piano", description: "very good", brand: "bosch", city: "London", price: 300 )
-trumpette = Instrument.new(available_from: "1889/03/03",name: "trumpette", description: "very good", brand: "bosch", city: "London", price: 300 )
-smajd = Instrument.new(available_from: "1889/03/03",name: "smajd", description: "very good", brand: "bosch", city: "London", price: 300 )
-portk = Instrument.new(available_from: "1889/03/03",name: "portk", description: "very good", brand: "bosch", city: "London", price: 300 )
+guitar = Instrument.create(available_from: "1889/03/03",name: "guitar", description: "very good", brand: "bosch", city: "London", price: 300, user_id: user_one.id )
+piano = Instrument.create(available_from: "1889/03/03",name: "piano", description: "very good", brand: "bosch", city: "London", price: 300, user_id: user_one.id  )
+trumpette = Instrument.create(available_from: "1889/03/03",name: "trumpette", description: "very good", brand: "bosch", city: "London", price: 300, user_id: user_one.id  )
+smajd = Instrument.create(available_from: "1889/03/03",name: "smajd", description: "very good", brand: "bosch", city: "London", price: 300, user_id: user_two.id )
+portk = Instrument.create(available_from: "1889/03/03",name: "portk", description: "very good", brand: "bosch", city: "London", price: 300, user_id: user_two.id )
 
 
-guitar.user = user_one
-guitar.save!
 
-piano.user = user_one
-piano.save!
-
-trumpette.user = user_one
-trumpette.save!
-
-smajd.user = user_two
-smajd.save!
-
-portk.user = user_two
-portk.save!
-
-
-booking_one = Booking.create(instrument_id: 1, user_id: 1, start_date: "1990/02/02", end_date:"1991/02/02")
-booking_two  = Booking.create(instrument_id: 2, user_id: 2, start_date: "1992/02/02", end_date:"1993/02/02")
+booking_one = Booking.create(instrument_id: instrument.id, user_id: user.id, start_date: "1990/02/02", end_date:"1991/02/02")
+booking_two  = Booking.create(instrument_id: instrument.id, user_id: user.id, start_date: "1992/02/02", end_date:"1993/02/02")
