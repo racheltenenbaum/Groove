45bc2c1ee38697c6c3462b19650a592e46237a85
Jayden Thelwell <jaydenthelwell02@gmail.com>
2023-08-24 12:20:05 +0100

    addingh mapping

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,6 +1,13 @@
 class InstrumentsController < ApplicationController
   def index
     @instruments = Instrument.all
+
+    @markers = @instruments.geocoded.map do |instrument|
+      {
+        lat: instrument.latitude,
+        lng: instrument.longitude
+      }
+    end
   end
 
   def show
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,9 +1,10 @@
 class Instrument < ApplicationRecord
   belongs_to :user
-
-
   # def self.owner_name
   #   User.find(instrument.user_id).username
   # end
   has_one_attached :photo
+
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
 end
