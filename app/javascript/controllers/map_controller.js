f9a62e87e62566cfb2c32abdefaeee24c9c9c722
Jayden Thelwell <jaydenthelwell02@gmail.com>
2023-08-25 16:12:46 +0100

    finalllll

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,6 +13,7 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-74.5, 40]
     });
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
@@ -33,7 +34,7 @@ export default class extends Controller {
     #fitMapToMarkers() {
       const bounds = new mapboxgl.LngLatBounds()
       this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000  })
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000k })
     }
 
   }
